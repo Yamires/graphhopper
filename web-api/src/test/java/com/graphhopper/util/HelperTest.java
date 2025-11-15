552a470fb018fdddbb077ac8c717425509cb20a4
Yamir A. Poldo Silva <yamira.poldosilva@MacBookPro-de-Yamir.local>
2025-11-15 16:04:29 -0500

    test gif rickroll

--- web-api/src/test/java/com/graphhopper/util/HelperTest.java
+++ web-api/src/test/java/com/graphhopper/util/HelperTest.java
@@ -67,66 +67,66 @@ public class HelperTest {
         assertEquals(1040, Helper.round(1041.02, -1), 1.e-7);
         assertEquals(1000, Helper.round(1041.02, -2), 1.e-7);
     }
-
-    @Test
-    public void testKeepIn() {
-        assertEquals(2, Helper.keepIn(2, 1, 4), 1e-2);
-        assertEquals(3, Helper.keepIn(2, 3, 4), 1e-2);
-        assertEquals(3, Helper.keepIn(-2, 3, 4), 1e-2);
-    }
-
-    @Test
-    public void testCamelCaseToUnderscore() {
-        assertEquals("test_case", Helper.camelCaseToUnderScore("testCase"));
-        assertEquals("test_case_t_b_d", Helper.camelCaseToUnderScore("testCaseTBD"));
-        assertEquals("_test_case", Helper.camelCaseToUnderScore("TestCase"));
-
-        assertEquals("_test_case", Helper.camelCaseToUnderScore("_test_case"));
-    }
-
-    @Test
-    public void testUnderscoreToCamelCase() {
-        assertEquals("testCase", Helper.underScoreToCamelCase("test_case"));
-        assertEquals("testCaseTBD", Helper.underScoreToCamelCase("test_case_t_b_d"));
-        assertEquals("TestCase_", Helper.underScoreToCamelCase("_test_case_"));
-    }
-
-    @Test
-    public void testIssue2609() {
-        String s = "";
-        for (int i = 0; i < 128; i++) {
-            s += "ä";
-        }
-
-        // all chars are 2 bytes so at 255 we cut the char into an invalid character and this is probably automatically
-        // corrected leading to a longer string (or do chars have special marker bits to indicate their byte length?)
-        assertEquals(257, new String(s.getBytes(UTF_CS), 0, 255, UTF_CS).getBytes(UTF_CS).length);
-
-        // see this in action:
-        byte[] bytes = "a".getBytes(UTF_CS);
-        assertEquals(1, new String(bytes, 0, 1, UTF_CS).getBytes(UTF_CS).length);
-        // force incorrect char:
-        bytes[0] = -25;
-        assertEquals(3, new String(bytes, 0, 1, UTF_CS).getBytes(UTF_CS).length);
-    }
-
-    @Test
-    void degreeToInt() {
-        int storedInt = 444_494_395;
-        double lat = Helper.intToDegree(storedInt);
-        assertEquals(44.4494395, lat);
-        assertEquals(storedInt, Helper.degreeToInt(lat));
-    }
-
-    @Test
-    void eleToInt() {
-        int storedInt = 1145636;
-        double ele = Helper.uIntToEle(storedInt);
-        // converting to double is imprecise
-        assertEquals(145.635986, ele, 1.e-6);
-        // ... but converting back to int should yield the same value we started with!
-        assertEquals(storedInt, Helper.eleToUInt(ele));
-    }
+//
+//    @Test
+//    public void testKeepIn() {
+//        assertEquals(2, Helper.keepIn(2, 1, 4), 1e-2);
+//        assertEquals(3, Helper.keepIn(2, 3, 4), 1e-2);
+//        assertEquals(3, Helper.keepIn(-2, 3, 4), 1e-2);
+//    }
+//
+//    @Test
+//    public void testCamelCaseToUnderscore() {
+//        assertEquals("test_case", Helper.camelCaseToUnderScore("testCase"));
+//        assertEquals("test_case_t_b_d", Helper.camelCaseToUnderScore("testCaseTBD"));
+//        assertEquals("_test_case", Helper.camelCaseToUnderScore("TestCase"));
+//
+//        assertEquals("_test_case", Helper.camelCaseToUnderScore("_test_case"));
+//    }
+//
+//    @Test
+//    public void testUnderscoreToCamelCase() {
+//        assertEquals("testCase", Helper.underScoreToCamelCase("test_case"));
+//        assertEquals("testCaseTBD", Helper.underScoreToCamelCase("test_case_t_b_d"));
+//        assertEquals("TestCase_", Helper.underScoreToCamelCase("_test_case_"));
+//    }
+//
+//    @Test
+//    public void testIssue2609() {
+//        String s = "";
+//        for (int i = 0; i < 128; i++) {
+//            s += "ä";
+//        }
+//
+//        // all chars are 2 bytes so at 255 we cut the char into an invalid character and this is probably automatically
+//        // corrected leading to a longer string (or do chars have special marker bits to indicate their byte length?)
+//        assertEquals(257, new String(s.getBytes(UTF_CS), 0, 255, UTF_CS).getBytes(UTF_CS).length);
+//
+//        // see this in action:
+//        byte[] bytes = "a".getBytes(UTF_CS);
+//        assertEquals(1, new String(bytes, 0, 1, UTF_CS).getBytes(UTF_CS).length);
+//        // force incorrect char:
+//        bytes[0] = -25;
+//        assertEquals(3, new String(bytes, 0, 1, UTF_CS).getBytes(UTF_CS).length);
+//    }
+//
+//    @Test
+//    void degreeToInt() {
+//        int storedInt = 444_494_395;
+//        double lat = Helper.intToDegree(storedInt);
+//        assertEquals(44.4494395, lat);
+//        assertEquals(storedInt, Helper.degreeToInt(lat));
+//    }
+//
+//    @Test
+//    void eleToInt() {
+//        int storedInt = 1145636;
+//        double ele = Helper.uIntToEle(storedInt);
+//        // converting to double is imprecise
+//        assertEquals(145.635986, ele, 1.e-6);
+//        // ... but converting back to int should yield the same value we started with!
+//        assertEquals(storedInt, Helper.eleToUInt(ele));
+//    }
 //
 //    /**
 //     * Nom du test : testEmptyString
